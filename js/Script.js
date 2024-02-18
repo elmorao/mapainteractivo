aa441ae27b6ceabf054e8b30674ea8bcbe0d7ddd
elmorao <127431381+elmorao@users.noreply.github.com>
2024-02-18 00:32:05 -0500

    mapa interactivo

---
+++ js/Script.js
@@ -0,0 +1,230 @@
+function abrirama(){
+    document.getElementById("Amazonas").style.display="block";
+}
+function cerrarama(){
+    document.getElementById("Amazonas").style.display="none";
+}
+
+function abrirant(){
+    document.getElementById("Antioquia").style.display="block";
+}
+function cerrarant(){
+    document.getElementById("Antioquia").style.display="none";
+}
+
+function abrirboy(){
+    document.getElementById("Boyaca").style.display="block";
+}
+function cerrarboy(){
+    document.getElementById("Boyaca").style.display="none";
+}
+
+function abrircor(){
+    document.getElementById("Cordoba").style.display="block";
+}
+function cerrarcor(){
+    document.getElementById("Cordoba").style.display="none";
+}
+
+function abrirsan(){
+    document.getElementById("Santander").style.display="block";
+}
+function cerrarsan(){
+    document.getElementById("Santander").style.display="none";
+}
+
+function abrirlag(){
+    document.getElementById("La guajira").style.display="block";
+}
+function cerrarlag(){
+    document.getElementById("La guajira").style.display="none";
+}
+
+function abrirsana(){
+    document.getElementById("San Andres y Providencia").style.display="block";
+}
+function cerrarsana(){
+    document.getElementById("San Andres y Providencia").style.display="none";
+}
+
+function abrircal(){
+    document.getElementById("Caldas").style.display="block";
+}
+function cerrarcal(){
+    document.getElementById("Caldas").style.display="none";
+}
+
+function abrircun(){
+    document.getElementById("Cundinamarca").style.display="block";
+}
+function cerrarcun(){
+    document.getElementById("Cundinamarca").style.display="none";
+}
+
+function abrirbog(){
+    document.getElementById("Bogota").style.display="block";
+}
+function cerrarbog(){
+    document.getElementById("Bogota").style.display="none";
+}
+
+function abrirqui(){
+    document.getElementById("Quindio").style.display="block";
+}
+function cerrarqui(){
+    document.getElementById("Quindio").style.display="none";
+}
+
+function abrirris(){
+    document.getElementById("Risaralda").style.display="block";
+}
+function cerrarris(){
+    document.getElementById("Risaralda").style.display="none";
+}
+
+function abrirtol(){
+    document.getElementById("Tolima").style.display="block";
+}
+function cerrartol(){
+    document.getElementById("Tolima").style.display="none";
+}
+
+function abrircaq(){
+    document.getElementById("Caqueta").style.display="block";
+}
+function cerrarcaq(){
+    document.getElementById("Caqueta").style.display="none";
+}
+
+function abrircau(){
+    document.getElementById("Cauca").style.display="block";
+}
+function cerrarcau(){
+    document.getElementById("Cauca").style.display="none";
+}
+
+function abrirhui(){
+    document.getElementById("Huila").style.display="block";
+}
+function cerrarhui(){
+    document.getElementById("Huila").style.display="none";
+}
+
+function abrirnar(){
+    document.getElementById("Nariño").style.display="block";
+}
+function cerrarnar(){
+    document.getElementById("Nariño").style.display="none";
+}
+
+function abrirput(){
+    document.getElementById("Putumayo").style.display="block";
+}
+function cerrarput(){
+    document.getElementById("Putumayo").style.display="none";
+}
+
+function abriratl(){
+    document.getElementById("Atlantico").style.display="block";
+}
+function cerraratl(){
+    document.getElementById("Atlantico").style.display="none";
+}
+
+function abrirbol(){
+    document.getElementById("Bolivar").style.display="block";
+}
+function cerrarbol(){
+    document.getElementById("Bolivar").style.display="none";
+}
+
+function abrirces(){
+    document.getElementById("Cesar").style.display="block";
+}
+function cerrarces(){
+    document.getElementById("Cesar").style.display="none";
+}
+
+function abrircho(){
+    document.getElementById("Choco").style.display="block";
+}
+function cerrarcho(){
+    document.getElementById("Choco").style.display="none";
+}
+
+function abrirmag(){
+    document.getElementById("Magdalena").style.display="block";
+}
+function cerrarmag(){
+    document.getElementById("Magdalena").style.display="none";
+}
+
+function abrirsuc(){
+    document.getElementById("Sucre").style.display="block";
+}
+function cerrarsuc(){
+    document.getElementById("Sucre").style.display="none";
+}
+
+function abrirara(){
+    document.getElementById("Arauca").style.display="block";
+}
+function cerrarara(){
+    document.getElementById("Arauca").style.display="none";
+}
+
+function abrirnors(){
+    document.getElementById("Norte de santader").style.display="block";
+}
+function cerrarnors(){
+    document.getElementById("Norte de santader").style.display="none";
+}
+
+function abrircas(){
+    document.getElementById("Casanare").style.display="block";
+}
+function cerrarcas(){
+    document.getElementById("Casanare").style.display="none";
+}
+
+function abrirval(){
+    document.getElementById("Valle del cauca").style.display="block";
+}
+function cerrarval(){
+    document.getElementById("Valle del cauca").style.display="none";
+}
+
+function abrirgua(){
+    document.getElementById("Guaviare").style.display="block";
+}
+function cerrargua(){
+    document.getElementById("Guaviare").style.display="none";
+}
+
+function abrirguai(){
+    document.getElementById("Guainia").style.display="block";
+}
+function cerrarguai(){
+    document.getElementById("Guainia").style.display="none";
+}
+
+function abrirmet(){
+    document.getElementById("Meta").style.display="block";
+}
+function cerrarmet(){
+    document.getElementById("Meta").style.display="none";
+}
+
+function abrirvau(){
+    document.getElementById("Vaupes").style.display="block";
+}
+function cerrarvau(){
+    document.getElementById("Vaupes").style.display="none";
+}
+
+function abrirvic(){
+    document.getElementById("Vichada").style.display="block";
+}
+function cerrarvic(){
+    document.getElementById("Vichada").style.display="none";
+}
